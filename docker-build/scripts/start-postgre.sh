27123fb6077d254da3412fa5e053637d6369f281
goflutterjava <goflutterjava@gmail.com>
2023-01-30 16:01:40 +0800

    fix data dir permission

--- docker-build/scripts/start-postgre.sh
+++ docker-build/scripts/start-postgre.sh
@@ -3,6 +3,7 @@
 POSTGRE_DATA=$POSTGRE_HOME/data
 POSTGRE_MATE=$POSTGRE_HOME/mate
 mkdir -p $POSTGRE_DATA
+chown -R postgres:postgres $POSTGRE_DATA
 # postgre need to init as non-root user
 su postgres -c "$POSTGRE_HOME/bin/initdb -U postgres -D $POSTGRE_DATA --no-locale --encoding=UTF8"
 cp $POSTGRE_MATE/conf/postgresql.conf  $POSTGRE_DATA/postgresql.conf
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module postgre_mate_go
 
-go 1.17
+go 1.18
 
 require (
 	github.com/gogf/gf v1.15.7
