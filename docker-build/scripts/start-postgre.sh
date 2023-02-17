8503846db017e6c557d5d26a6f1482a55c71ac45
Lico-Tom <583566138@qq.com>
2023-02-17 14:34:31 +0800

    fix add env bug.

--- docker-build/scripts/start-postgre.sh
+++ docker-build/scripts/start-postgre.sh
@@ -3,10 +3,17 @@
 POSTGRE_DATA=$POSTGRE_HOME/data
 POSTGRE_MATE=$POSTGRE_HOME/mate
 HBA_CONF=$POSTGRE_DATA/pg_hba.conf
-echo 'local   all             all                                     trust' >$HBA_CONF
-echo 'host    all             all             127.0.0.1/32            trust' >>$HBA_CONF
 mkdir -p $POSTGRE_DATA
 chown -R postgres:postgres $POSTGRE_DATA
+echo 'local   all             all                                     trust' >$HBA_CONF
+echo 'host    all             all             127.0.0.1/32            trust' >>$HBA_CONF
+echo "host    all             all             ::1/128                 trust" >>$HBA_CONF
+echo "local   replication     all                                     trust" >>$HBA_CONF
+echo "host    replication     all             127.0.0.1/32            trust" >>$HBA_CONF
+echo "host    replication     all             ::1/128                 trust" >>$HBA_CONF
+echo "host    replication     all             ::1/128                 trust" >>$HBA_CONF
+echo "host    all             all              0.0.0.0/0              ${REMOTE_AUTH_TYPE:-trust}"   >>$HBA_CONF
+echo "host    all             all              ::/0                   ${REMOTE_AUTH_TYPE:-trust}"   >>$HBA_CONF
 # postgre need to init as non-root user
 su postgres -c "$POSTGRE_HOME/bin/initdb -U postgres -D $POSTGRE_DATA --no-locale --encoding=UTF8"
 cp $POSTGRE_MATE/conf/postgresql.conf  $POSTGRE_DATA/postgresql.conf
