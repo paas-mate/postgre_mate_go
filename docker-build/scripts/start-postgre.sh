0cfa6d23fe701970a989456d977cee98e1a56829
TingTing Wang <1922919664@qq.com>
2023-02-26 08:57:41 +0800

    use postgres user (#4)

--- docker-build/scripts/start-postgre.sh
+++ docker-build/scripts/start-postgre.sh
@@ -4,7 +4,6 @@ POSTGRE_DATA=$POSTGRE_HOME/data
 POSTGRE_MATE=$POSTGRE_HOME/mate
 HBA_CONF=$POSTGRE_DATA/pg_hba.conf
 mkdir -p $POSTGRE_DATA
-chown -R postgres:postgres $POSTGRE_DATA
 echo 'local   all             all                                     trust' >$HBA_CONF
 echo 'host    all             all             127.0.0.1/32            trust' >>$HBA_CONF
 echo "host    all             all             ::1/128                 trust" >>$HBA_CONF
@@ -15,8 +14,8 @@ echo "host    replication     all             ::1/128                 trust" >>$
 echo "host    all             all              0.0.0.0/0              ${REMOTE_AUTH_TYPE:-trust}"   >>$HBA_CONF
 echo "host    all             all              ::/0                   ${REMOTE_AUTH_TYPE:-trust}"   >>$HBA_CONF
 # postgre need to init as non-root user
-su postgres -c "$POSTGRE_HOME/bin/initdb -U postgres -D $POSTGRE_DATA --no-locale --encoding=UTF8"
+$POSTGRE_HOME/bin/initdb -U postgres -D $POSTGRE_DATA --no-locale --encoding=UTF8
 cp $POSTGRE_MATE/conf/postgresql.conf  $POSTGRE_DATA/postgresql.conf
 
-su postgres -c "$POSTGRE_HOME/bin/pg_ctl -D $POSTGRE_DATA start"
+$POSTGRE_HOME/bin/pg_ctl -D $POSTGRE_DATA start
 psql -U postgres -f $POSTGRE_MATE/sql/init_database.sql
